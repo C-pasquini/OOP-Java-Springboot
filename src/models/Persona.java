f19295e75be9256a7d8f98f5d3a8136fd7e5f720
Pasquini Camillo <cpasquini@itsmeccanicabruzzo.eiu>
2023-06-19 10:31:51 +0200

    Aggiunti nome e cognome e costruttore a Persona

--- src/models/Persona.java
+++ src/models/Persona.java
@@ -1,5 +1,11 @@
 package models;
 
 public class Persona {
+    public Persona(String nome, String cognome){
+        this.nome = nome;
+        this.cognome = cognome;
+    }
 
+    String nome;
+    String cognome;
 }
