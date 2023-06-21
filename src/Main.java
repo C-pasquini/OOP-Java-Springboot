37dd4ab98e8b9c705f1d969cf271002e4e46636f
Pasquini Camillo <cpasquini@itsmeccanicabruzzo.eiu>
2023-06-21 09:29:30 +0200

    added stuff to main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Car macchina = new Car();
 
-
+        macchina.year = 1960;
+        System.out.println(macchina.year);
     }
 }
