8b48450ed824e4bd39e055a96d3b4eeea92b2545
Pasquini Camillo <cpasquini@itsmeccanicabruzzo.eiu>
2023-06-21 11:59:46 +0200

    corrected the wrong year for the car model (it was 1960, FIAT punto was manufactured since 1993)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car();
 
-        car.year = 1960; // Year is a public variable, meaning that we can access it directly without problems
+        car.year = 1993; // Year is a public variable, meaning that we can access it directly without problems
         car.manufacturer = "FIAT"; /* Manufacturer is a protected variable,
                                     * Meaning we can access it only if the class exists in the same packag (meaning that we can access it even on different files, but they have to be in the same package)*/
         car.model = "Punto"; // Model is a private variable of the class, meaning we can't access it outside of where the class is defined (that is if the class is defined inside a specific file, we can access it only in said file
