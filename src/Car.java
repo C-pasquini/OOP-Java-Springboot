8772c3117a1ca8ba5e6bfd4be8ecf428b8fda1a6
Pasquini Camillo <cpasquini@itsmeccanicabruzzo.eiu>
2023-06-21 11:58:12 +0200

    structured the project in it's proper way

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,28 @@ public class Car {
     private String model;
     protected String manufacturer;
     public int year;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Car macchina = new Car();
+        Car car = new Car();
 
-        macchina.year = 1960;
-        System.out.println(macchina.year);
+        car.year = 1960; // Year is a public variable, meaning that we can access it directly without problems
+        car.manufacturer = "FIAT"; /* Manufacturer is a protected variable,
+                                    * Meaning we can access it only if the class exists in the same packag (meaning that we can access it even on different files, but they have to be in the same package)*/
+        car.model = "Punto"; // Model is a private variable of the class, meaning we can't access it outside of where the class is defined (that is if the class is defined inside a specific file, we can access it only in said file
+        car.setModel("Punto"); // This is the way we can access the "model" variable
     }
 }
